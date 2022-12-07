7423de9f28f594900e89311240ea0e8d7c4afd3c
robin-msk <robin.musyoki@kibo.school>
2022-12-07 04:38:01 +0300

    added bew files

---
+++ script.js
@@ -0,0 +1,9 @@
+var pic = document.getElementById("first");
+const initialPic=pic ;
+
+pic.addEventListener("click,() => {")
+
+function modifyPic(new_Pic){
+    const selected =document.getElementsByClassName(".selected")
+    
+}
